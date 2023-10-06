f6c5e67efab83113107ecdae576564897390cd58
gm <146473511+Pooh1206@users.noreply.github.com>
2023-10-06 20:44:50 +0000

    Update 4 files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,39 @@
-// https://nuxt.com/docs/guide/directory-structure/nuxt.config
-export default defineNuxtConfig({});
+{
+    "name": "nuxt-app",
+    "private": true,
+    "type": "module",
+    "scripts": {
+      "build": "nuxt build",
+      "dev": "nuxt dev",
+      "generate": "nuxt generate",
+      "preview": "nuxt preview",
+      "postinstall": "nuxt prepare"
+    },
+    "devDependencies": {
+      "@nuxt/devtools": "latest",
+      "@nuxtjs/color-mode": "^3.2.0",
+      "@nuxtjs/ionic": "^0.12.1",
+      "@nuxtjs/tailwindcss": "^6.8.0",
+      "@tailwindcss/forms": "^0.5.3",
+      "@vueuse/core": "^9.12.0",
+      "@vueuse/nuxt": "^9.12.0",
+      "less": "^4.1.3",
+      "nuxt": "^3.7.4",
+      "nuxt-icon": "^0.2.10",
+      "sass": "^1.68.0",
+      "vue": "^3.3.4",
+      "vue-router": "^4.2.5"
+    },
+    "dependencies": {
+      "@headlessui/vue": "^1.7.8",
+      "@vant/nuxt": "^1.0.0",
+      "firebase": "^10.4.0",
+      "floating-vue": "^2.0.0-beta.20",
+      "js-base64": "^3.7.4",
+      "nuxt-vuefire": "^0.4.0",
+      "vant": "^4.0.0",
+      "vuedraggable": "^4.1.0",
+      "vuefire": "^3.1.17"
+    }
+  }
+  
---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+
+import Me_movie from '~/components/Me_movie.vue';
+<template>
+    <IonPage>
+        <IonContent class="ion-padding">
+          <IonButton> Logout </IonButton>
+            <Profile />
+            <Ioncard>
+              <Me_coupon />
+            </Ioncard>
+
+            <van-row class="user-links">
+      <van-col span="6">
+        <van-icon name="pending-payment" />
+        
+        待付款
+      </van-col>
+      <van-col span="6">
+        <van-icon name="records" />
+        待接单
+      </van-col>
+      <van-col span="6">
+        <van-icon name="tosend" />
+        待发货
+      </van-col>
+      <van-col span="6">
+        <van-icon name="logistics" />
+        已发货
+      </van-col>
+    </van-row>
+    <Me_movie />
+        </IonContent>
+    
+        <Tabbar2 />
+        <Clearpage />
+    </IonPage>
+
+
+</template>
+
+<style lang="less">
+body {
+  margin: 0;
+  font-size: 16px;
+  background-color: #f8f8f8;
+  -webkit-font-smoothing: antialiased;
+}
+
+.user {
+  &-poster {
+    width: 100%;
+    height: 53vw;
+    display: block;
+  }
+
+  &-group {
+    margin-bottom: 15px;
+  }
+
+  &-links {
+    padding: 15px 0;
+    font-size: 12px;
+    text-align: center;
+    background-color: #fff;
+
+    .van-icon {
+      display: block;
+      font-size: 24px;
+    }
+  }
+}
+</style>
